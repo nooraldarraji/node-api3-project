3c3ed6e755aaccfc12204d6e58e7832acc99810e
Noor Al Darraji <49676151+nooraldarraji@users.noreply.github.com>
2020-05-07 20:32:48 -0500

    created the server and completed logger function :hammer:

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code away!
+const express = require('express')
+
+const myServer = require('./server')
+const server = myServer.server;
+const logger = myServer.logger;
+
+const postsRouter = require('./posts/postRouter')
+const usersRouter = require('./users/userRouter')
+
+
+server.use(express.json())
+
+
+server.use('/api/posts', logger, postsRouter)
+server.use('/api/users', logger, usersRouter)
+
+const port = 6000;
+server.listen(port, () => { console.log(`\n == Server up and running on port: ${port} == \n`) })
--- server.js
+++ server.js
@@ -2,12 +2,40 @@ const express = require('express');
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+server.get('/', logger, (req, res) => {
+
+  res.status(200).send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const colors = {
+    "noColor": "\033[0m",
+    "red": "\033[1;31m",
+    "green": "\033[1;32m",
+    "yellow": "\033[1;33m",
+    "blue": "\033[1;34m"
+  }
 
-module.exports = server;
+  let code = res.statusCode
+
+  if (code.toString()[0] === '2') {
+    console.log(`${req.method} ${req.url} ${colors.green}${code}${colors.noColor}`)
+  } else if (code.toString()[0] === '3') {
+    console.log(`${req.method} ${req.url} ${colors.blue}${code}${colors.noColor}`)
+  } else if (code.toString()[0] === '4') {
+    console.log(`${req.method} ${req.url} ${colors.yellow}${code}${colors.noColor}`)
+  } else if (code.toString()[0] === '5') {
+    console.log(`${req.method} ${req.url} ${colors.red}${code}${colors.noColor}`)
+  } else {
+    console.log(`${req.method} ${req.url} ${code}`)
+  }
+
+  next()
+}
+
+module.exports = {
+  server: server,
+  logger: logger
+}
