11aca4587cbd9c27a3f4fb328b51fbbf911c46c0
Noor Al Darraji <49676151+nooraldarraji@users.noreply.github.com>
2020-05-07 20:33:27 -0500

    created 3 middleware for validation :hammer:

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('./userDb')
 
 const router = express.Router();
 
@@ -34,14 +35,40 @@ router.put('/:id', (req, res) => {
 
 function validateUserId(req, res, next) {
   // do your magic!
+  const id = req.params.id
+  const user = req.user
+
+  if (!id) {
+    res.status(400).json({ message: "invalid user id" })
+  } else {
+    db
+      .insert(user)
+      .then((user) => {
+        res.send(201).json(user)
+      })
+      .catch(() => {
+        res.send(500).json({ error: "error" })
+      })
+  }
+
 }
 
 function validateUser(req, res, next) {
   // do your magic!
+  if (!req.body) {
+    res.status(400).json({ message: "missing user data" })
+  } else if (!req.body.name) {
+    res.status(400).json({ message: "missing required name field" })
+  }
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  if (!req.body) {
+    res.status(400).json({ message: "missing post data" })
+  } else if (!req.body.text) {
+    res.status(400).json({ message: "missing required text field" })
+  }
 }
 
 module.exports = router;
