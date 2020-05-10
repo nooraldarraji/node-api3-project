d054e249948990f0c6b3a7c35bcf9852e50c47f2
Noor Al Darraji <49676151+nooraldarraji@users.noreply.github.com>
2020-05-10 01:49:56 -0500

    completed the user route :hammer:

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,34 +1,107 @@
 const express = require('express');
-const db = require('./userDb')
-
+const Users = require('./userDb')
+const Posts = require('../posts/postDb')
+const myServer = require('../server')
+// const logger = myServer.logger
 const router = express.Router();
 
-router.post('/', (req, res) => {
+
+router.post('/', validateUser, (req, res) => {
   // do your magic!
+  const name = req.body.name
+  Users
+    .insert({ name })
+    .then((post) => {
+      res.status(201).json(post)
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err })
+    })
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // do your magic!
+  const id = req.params.id
+  const text = req.body.text
+  Posts
+    .insert({ user_id: id, text: text })
+    .then((post) => {
+      res.status(201).json(post)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({ error: err })
+    })
 });
 
 router.get('/', (req, res) => {
   // do your magic!
+  Users.get()
+    .then((user) => {
+      res.status(200).json(user)
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err })
+    })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
   // do your magic!
+  const id = req.params.id
+  Users.getById(id)
+    .then((user) => {
+      res.status(200).json(user)
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err })
+    })
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
   // do your magic!
+  const id = req.params.id
+  Posts
+    .getById(id)
+    .then((post) => {
+      res.status(201).json(post)
+    })
+    .catch((err) => {
+      // console.log(err)
+      res.status(500).json({ error: err })
+    })
 });
 
 router.delete('/:id', (req, res) => {
   // do your magic!
+  const id = req.params.id
+  Users.getById(id)
+    .then(user => {
+      if (user) {
+        Users.remove(id)
+          .then((u) => {
+            res.status(200).json({ recordesDeleted: u })
+          })
+          .catch((err) => {
+            res.status(500).json({ error: err })
+          })
+      } else {
+        res.status(404).json({ error: `User id is not found` })
+      }
+    })
 });
 
 router.put('/:id', (req, res) => {
   // do your magic!
+  const id = req.params.id
+  const name = req.body.name
+  Users
+    .update(id, { name })
+    .then(post => {
+      res.status(200).json(post)
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: err })
+    })
 });
 
 //custom middleware
@@ -36,21 +109,11 @@ router.put('/:id', (req, res) => {
 function validateUserId(req, res, next) {
   // do your magic!
   const id = req.params.id
-  const user = req.user
-
   if (!id) {
-    res.status(400).json({ message: "invalid user id" })
+    res.status(417).json({ message: "invalid user id" })
   } else {
-    db
-      .insert(user)
-      .then((user) => {
-        res.send(201).json(user)
-      })
-      .catch(() => {
-        res.send(500).json({ error: "error" })
-      })
+    next()
   }
-
 }
 
 function validateUser(req, res, next) {
@@ -58,7 +121,17 @@ function validateUser(req, res, next) {
   if (!req.body) {
     res.status(400).json({ message: "missing user data" })
   } else if (!req.body.name) {
-    res.status(400).json({ message: "missing required name field" })
+    res.status(417).json({ message: "missing required name field" })
+  } else {
+    Users.get()
+      .then(users => {
+        const user = users.find(user => user.name === req.body.name)
+        if (user) {
+          res.status(400).json({ message: ` ${user.name} is already taken.` })
+        } else {
+          next()
+        }
+      })
   }
 }
 
@@ -68,6 +141,8 @@ function validatePost(req, res, next) {
     res.status(400).json({ message: "missing post data" })
   } else if (!req.body.text) {
     res.status(400).json({ message: "missing required text field" })
+  } else {
+    next()
   }
 }
 
