ed02d684836edb4c6ed01ea0a5c2f670be23c860
Noor Al Darraji <49676151+nooraldarraji@users.noreply.github.com>
2020-05-10 03:13:43 -0500

    Finshed posts route, practice has been completed. :fire:

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,78 @@
 const express = require('express');
+const Posts = require('./postDb')
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   // do your magic!
+  Posts
+    .get()
+    .then(posts => {
+      if (posts) {
+        res.status(200).json(posts)
+      } else {
+        res.status(500).json({ error: 'No posts found' })
+      }
+    })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validatePostId, (req, res) => {
   // do your magic!
+  const id = req.params.id
+  Posts
+    .getById(id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post)
+      } else {
+        res.status(500).json({ error: 'No post found ' })
+      }
+    })
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validatePostId, (req, res) => {
   // do your magic!
+  const id = req.params.id
+  Posts
+    .remove(id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post)
+      } else {
+        res.status(500).json({ error: 'No post found ' })
+      }
+    })
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validatePostId, (req, res) => {
   // do your magic!
+  const id = req.params.id
+  const text = req.body.text
+  Posts
+    .update(id, { text })
+    .then(post => {
+      if (post) {
+        res.status(200).json(post)
+      } else {
+        res.status(500).json({ error: 'No post found ' })
+      }
+    })
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
   // do your magic!
+  const id = req.params.id
+  Posts
+    .getById(id)
+    .then(id => {
+      if (id) {
+        next()
+      } else {
+        res.status(404).json({ error: 'No id found for this post' })
+      }
+    })
 }
 
 module.exports = router;
